619a5ec946d4dfd7741d0565c32a509a46c99285
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-16 22:29:53 +0530

    Connected to the database

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 const express = require('express');
 const path = require('path')
+const mongoose = require('mongoose');
+const e = require('express');
+
+mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true,useUnifiedTopology: true  });
+let db = mongoose.connection;
 
 
+//check connection
+db.once('open', () => {
+    console.log('Connected to mongodb')
+})
+
+
+// check for DB errors
+db.on('error', (err) => {
+    console.log(err);
+})
+
+
+// init app
 const app = express();
 
+
+//bring in models
+let Article = require('./models/article')
+
+
+//load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+//Home route
 app.get('/', (req, res) => {
-    let articles = [
-        {
-            id: 1,
-            title: 'Article One',
-            author: 'Atul Sharma',
-            body: 'This is article one'
-        },
-        {
-            id: 2,
-            title: 'Article two',
-            author: 'Atul Sharma',
-            body: 'This is article two'
-        },
-        {
-            id: 3,
-            title: 'Article three',
-            author: 'Atul Sharma',
-            body: 'This is article three'
+    Article.find({}, (err, articles) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
         }
-    ];
-    res.render('index', {
-        title: 'Articles',
-        articles: articles
-    })
+    });
 });
 
 app.get('/articles/add', (req, res) => {
@@ -40,6 +52,8 @@ app.get('/articles/add', (req, res) => {
     })
 })
 
+
+//start server
 app.listen(3000, () => {
     console.log('Server is running on 3000')
 });
